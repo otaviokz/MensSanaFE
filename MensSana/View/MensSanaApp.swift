c1fe6e4488c4c70de1b2d67e374766b640da4770
Otavio Zabaleta <otaviokz@gmail.com>
2023-04-25 13:11:14 -0300

    icons

---
+++ MensSana/View/MensSanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MensSanaApp.swift
+//  MensSana
+//
+//  Created by Otávio Zabaleta on 25/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MensSanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
